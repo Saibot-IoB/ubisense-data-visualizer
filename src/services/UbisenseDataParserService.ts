aba0d1ca8319b2215e37d0a888932985e86649cf
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-03-05 20:19:06 +0100

    Refactor UbisenseDataParserService

--- src/services/UbisenseDataParserService.ts
+++ src/services/UbisenseDataParserService.ts
@@ -1,149 +1,148 @@
+import { TimeConverter } from "../util/Converters/TimeConverter";
+
 export type ExtractedDataType = {
-  time: string;
-  tag: string;
-  x: string;
-  y: string;
+    time: string;
+    tag: string;
+    x: string;
+    y: string;
 };
 
 type BubbleChartDataType = {
-  x: number;
-  y: number;
-  r: number;
+    x: number;
+    y: number;
+    r: number;
 };
 
 export type DatasetType = {
-  label: string;
-  data: BubbleChartDataType[];
-  backgroundColor: string;
+    label: string;
+    data: BubbleChartDataType[];
+    backgroundColor: string;
 }[];
 
+const TagToEntityMap: Record<string, string> = {
+    "00:11:CE:00:00:00:CB:34": "Participant 5",
+    "00:11:CE:00:00:00:CB:23": "Participant 4",
+    "00:11:CE:00:00:00:CB:2B": "Participant 3",
+    "00:11:CE:00:00:00:CB:29": "Participant 2",
+    "00:11:CE:00:00:00:CB:2E": "Participant 1",
+    "00:11:CE:00:00:00:CB:35": "4701",
+    "00:11:CE:00:00:00:CB:25": "4702",
+    "00:11:CE:00:00:00:CB:2C": "4703",
+    "00:11:CE:00:00:00:CB:33": "White",
+};
+
+const EntityColors: string[] = [
+    "rgba(233, 30, 99), 0.5)",
+    "rgba(156, 39, 17, 0.5)",
+    "rgba(3, 169, 24, 0.5)",
+    "rgba(0, 150, 136, 0.5)",
+    "rgba(255, 235, 59, 0.5)",
+    "rgba(255, 152, 0, 0.5)",
+    "rgba(255, 87, 34, 0.5)",
+    "rgba(121, 85, 72, 0.5)",
+    "rgba(96, 125, 13, 0.5)",
+];
+
 export class UbisenseDataParserService {
-  public static dataCache: Map<string, ExtractedDataType[]> = new Map<string,ExtractedDataType[]>();
-  public static participant5: BubbleChartDataType[] = [];
-  public static participant4: BubbleChartDataType[] = [];
-  public static robot1: BubbleChartDataType[] = [];
+    public static dataCache: Map<string, ExtractedDataType[]> = new Map<string, ExtractedDataType[]>();
+    public static participant5: BubbleChartDataType[] = [];
+    public static participant4: BubbleChartDataType[] = [];
+    public static robot1: BubbleChartDataType[] = [];
 
-  public static parseData(data: string, valid: boolean) {
-    const lines: string[] = data.split("\n");
-    lines.shift();
+    public static parseData(data: string, valid: boolean) {
+        const lines: string[] = data.split("\n");
+        lines.shift();
 
-    lines.forEach((line: string) => {
-      line = line.replace(/\0/g, "");
+        lines.forEach((line: string) => {
+            line = line.replace(/\0/g, "");
 
-      const time: string = line.substring(7, 15);
-      const tag: string = line.substring(21, 44);
-      const dataValid: string = line.substring(53, 55);
-      const x: string = line.substring(63, 68).trim();
-      const y: string = line.substring(71, 76).trim();
-      // variance has index (90, 94)
+            const time: string = line.substring(7, 15);
+            const tag: string = line.substring(21, 44);
+            const dataValid: string = line.substring(53, 55);
+            const x: string = line.substring(63, 68).trim();
+            const y: string = line.substring(71, 76).trim();
+            // variance has index (90, 94)
 
-      if (valid ? dataValid == "ok" : dataValid != "ok") {
-        if (this.dataCache.has(tag)) {
-          this.dataCache.get(tag)?.push({ time, tag, x, y });
-        } else {
-          this.dataCache.set(tag, [{ time, tag, x, y }]);
-        }
-      }
-    });
-  }
+            if (valid ? dataValid == "ok" : dataValid != "ok") {
+                if (this.dataCache.has(tag)) {
+                    this.dataCache.get(tag)?.push({ time, tag, x, y });
+                } else {
+                    this.dataCache.set(tag, [{ time, tag, x, y }]);
+                }
+            }
+        });
+    }
 
-  public static GetParsedData() {
-    return this.dataCache;
-  }
+    public static GetParsedData() {
+        return this.dataCache;
+    }
 
-  public static GetExperimentDuration(): { start: number; end: number } {
-    const timeArr: number[] = [];
+    public static GetExperimentDuration(): { start: number; end: number } {
+        const timeArr: number[] = [];
 
-    this.dataCache.forEach((value, key) => {
-      value.forEach((e: ExtractedDataType) => {
-        const timeArrStr = e.time.split(":");
-        timeArr.push(
-          parseInt(timeArrStr[0]) * 60 * 60 +
-            parseInt(timeArrStr[1]) * 60 +
-            parseInt(timeArrStr[2])
-        );
-      });
-    });
+        this.dataCache.forEach((value, key) => {
+            value.forEach((e: ExtractedDataType) => {
+                const timeArrStr = e.time.split(":");
+                timeArr.push(
+                    parseInt(timeArrStr[0]) * 60 * 60 +
+                    parseInt(timeArrStr[1]) * 60 +
+                    parseInt(timeArrStr[2])
+                );
+            });
+        });
 
-    return { start: 0, end: Math.max(...timeArr) - Math.min(...timeArr) };
-  }
+        return { start: 0, end: Math.max(...timeArr) - Math.min(...timeArr) };
+    }
 
-  public static GetExperimentInterval(): { start: number; end: number } {
-    const timeArr: number[] = [];
+    public static GetExperimentInterval(): { start: number; end: number } {
+        const timeArr: number[] = [];
 
-    this.dataCache.forEach((value, key) => {
-      value.forEach((e: ExtractedDataType) => {
-        const timeArrStr = e.time.split(":");
-        timeArr.push(
-          parseInt(timeArrStr[0]) * 60 * 60 +
-            parseInt(timeArrStr[1]) * 60 +
-            parseInt(timeArrStr[2])
-        );
-      });
-    });
+        this.dataCache.forEach((value, key) => {
+            value.forEach((e: ExtractedDataType) => {
+                const timeArrStr = e.time.split(":");
+                timeArr.push(
+                    parseInt(timeArrStr[0]) * 60 * 60 +
+                    parseInt(timeArrStr[1]) * 60 +
+                    parseInt(timeArrStr[2])
+                );
+            });
+        });
 
-    return { start: Math.min(...timeArr), end: Math.max(...timeArr) };
-  }
+        return { start: Math.min(...timeArr), end: Math.max(...timeArr) };
+    }
 
-  public static GetBubbleChartDatasets(
-    rangeStart: number,
-    rangeEnd: number
-  ): DatasetType {
-    let index = 0;
-    const datasets: DatasetType = [];
+    public static GetBubbleChartDatasets(rangeStart: number, rangeEnd: number): DatasetType {
+        let index = 0;
+        const datasets: DatasetType = [];
 
-    const mapping: Record<string, string> = {
-      "00:11:CE:00:00:00:CB:34": "Participant 5",
-      "00:11:CE:00:00:00:CB:23": "Participant 4",
-      "00:11:CE:00:00:00:CB:2B": "Participant 3",
-      "00:11:CE:00:00:00:CB:29": "Participant 2",
-      "00:11:CE:00:00:00:CB:2E": "Participant 1",
-      "00:11:CE:00:00:00:CB:35": "4701",
-      "00:11:CE:00:00:00:CB:25": "4702",
-      "00:11:CE:00:00:00:CB:2C": "4703",
-      "00:11:CE:00:00:00:CB:33": "White",
-    };
+        this.dataCache.forEach((value, key) => {
+            const currentArr: BubbleChartDataType[] = [];
+            const experimentStart = this.GetExperimentInterval().start;
 
-    const colors: string[] = [
-      "rgba(233, 30, 99), 0.5)",
-      "rgba(156, 39, 17, 0.5)",
-      "rgba(3, 169, 24, 0.5)",
-      "rgba(0, 150, 136, 0.5)",
-      "rgba(255, 235, 59, 0.5)",
-      "rgba(255, 152, 0, 0.5)",
-      "rgba(255, 87, 34, 0.5)",
-      "rgba(121, 85, 72, 0.5)",
-      "rgba(96, 125, 13, 0.5)",
-    ];
+            value.forEach((extractedData: ExtractedDataType) => {
+                const timeInSeconds = TimeConverter.convertTimestampToSeconds(extractedData.time);
 
-    this.dataCache.forEach((value, key) => {
-      const currentArr: BubbleChartDataType[] = [];
-      const experimentStart = this.GetExperimentInterval().start;
+                if (
+                    timeInSeconds >= (rangeStart + experimentStart) &&
+                    timeInSeconds <= (rangeEnd + experimentStart)
+                ) {
+                    currentArr.push({
+                        x: parseFloat(extractedData.x),
+                        y: parseFloat(extractedData.y),
+                        r: 3
+                    });
+                }
+            });
 
-      value.forEach((e: ExtractedDataType) => {
-        const timeArrStr = e.time.split(":");
-        const timeInSeconds =
-          parseInt(timeArrStr[0]) * 60 * 60 +
-          parseInt(timeArrStr[1]) * 60 +
-          parseInt(timeArrStr[2]);
+            datasets.push({
+                label: TagToEntityMap[key],
+                data: currentArr,
+                backgroundColor: EntityColors[index],
+            });
 
-        if (
-          timeInSeconds >= rangeStart + experimentStart &&
-          timeInSeconds <= rangeEnd + experimentStart
-        ) {
-          currentArr.push({ x: parseFloat(e.x), y: parseFloat(e.y), r: 3 });
-        }
-      });
+            index++;
+        });
 
-      datasets.push({
-        label: mapping[key],
-        data: currentArr,
-        backgroundColor: colors[index],
-      });
-
-      index++;
-    });
-
-    return datasets;
-  }
+        return datasets;
+    }
 }
